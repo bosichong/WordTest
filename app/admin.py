4d39e9c7295a218ee48fa7ac9c626e285c7baac5
J_sky <bosichong@qq.com>
2020-02-08 14:27:55 +0800

    修复答题空单词提示信息

--- app/admin.py
+++ app/admin.py
@@ -3,21 +3,21 @@ from django.contrib import admin
 from django.utils.safestring import mark_safe
 from django.contrib.auth.admin import UserAdmin
 
-from .models import UserProfile,Word,Category,Questionbank,Examination_results,Answer_record
+from .models import UserProfile, Word, Category, Questionbank, Examination_results, Answer_record
 
 
-#通过继承UserAdmin来解决扩展用户后修改密码明文问题
+# 通过继承UserAdmin来解决扩展用户后修改密码明文问题
 class UserProfileAdmin(UserAdmin):
     """用来显示用户相关"""
     # 用来显示用户字段
-    list_display = ('username', 'user_nick_name', 'email', 'user_gender' )
+    list_display = ('username', 'user_nick_name', 'email', 'user_gender')
 
     fieldsets = (
         (None, {'fields': ('username', 'password', 'first_name', 'last_name', 'email')}),
-        (('User Information'), {'fields': ('user_nick_name', 'user_gender', 'user_birday', 'user_detail','user_image')}),
+        (('User Information'),
+         {'fields': ('user_nick_name', 'user_gender', 'user_birday', 'user_detail', 'user_image')}),
         (('Permissions'), {'fields': ('is_superuser', 'is_staff', 'is_active',
-                                                  'groups', 'user_permissions')}),
-
+                                      'groups', 'user_permissions')}),
         (('Important dates'), {'fields': ('last_login', 'date_joined')}),
 
     )
@@ -29,12 +29,12 @@ class UserProfileAdmin(UserAdmin):
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name','detail','sort_id')
-    #过滤器设置
-    list_filter = ('name','sort_id')
+    list_display = ('name', 'detail', 'sort_id')
+    # 过滤器设置
+    list_filter = ('name', 'sort_id')
+
 
 class WordAdmin(admin.ModelAdmin):
-
     # def has_change_permission(self, request, obj=None):
     #     has_class_permission = super(WordAdmin, self).has_change_permission(request, obj)
     #     if not has_class_permission:
@@ -53,26 +53,22 @@ class WordAdmin(admin.ModelAdmin):
     #         obj.word_user = request.user
     #         super().save_model(request, obj, form, change)
 
-
-
-
-    list_display = ('word','ch_meaning','word_user','word_tag',)
+    list_display = ('word', 'ch_meaning', 'word_user', 'word_tag',)
     filter_horizontal = ('categorys',)
     # ordering设置默认排序字段，负号表示降序排序
     ordering = ('-word_create_time',)
     radio_fields = {"istype": admin.VERTICAL}
 
-    #筛选器
-    list_filter =('categorys',) #过滤器
-    search_fields =('word', ) #搜索字段
-    date_hierarchy = 'word_create_time'    # 详细时间分层筛选　
-
+    # 筛选器
+    list_filter = ('categorys',)  # 过滤器
+    search_fields = ('word',)  # 搜索字段
+    date_hierarchy = 'word_create_time'  # 详细时间分层筛选　
 
 
 class QuestionbankAdmin(admin.ModelAdmin):
-    exclude = ('q_user',)#隐藏字段，自动填充
+    exclude = ('q_user',)  # 隐藏字段，自动填充
 
-    #显示自己添加的内容
+    # 显示自己添加的内容
     def get_queryset(self, request):
         qs = super().get_queryset(request)
         if request.user.is_superuser:
@@ -84,34 +80,32 @@ class QuestionbankAdmin(admin.ModelAdmin):
         obj.q_user = request.user
         super().save_model(request, obj, form, change)
 
-
-    list_display = ('name','detail','isopen','q_user')
-    #过滤器设置
+    list_display = ('name', 'detail', 'isopen', 'q_user')
+    # 过滤器设置
     list_filter = ('name',)
     filter_horizontal = ('words',)
 
-    date_hierarchy = 'q_create_time'    # 详细时间分层筛选　
+    date_hierarchy = 'q_create_time'  # 详细时间分层筛选　
 
 
 class Examination_resultsAdmin(admin.ModelAdmin):
-    list_display = ('questionbank','score','score','qbtime','e_create_time','e_user',)
-    #过滤器设置
-    list_filter = ('questionbank','e_user')
-
+    list_display = ('questionbank', 'score', 'score', 'qbtime', 'e_create_time', 'e_user',)
+    # 过滤器设置
+    list_filter = ('questionbank', 'e_user')
 
 
 class Answer_recordAdmin(admin.ModelAdmin):
-    list_display = ('word','isTrue',)
-    #过滤器设置
-    list_filter = ('word','isTrue')
+    list_display = ('word', 'isTrue',)
+    # 过滤器设置
+    list_filter = ('word', 'isTrue')
 
 
 admin.site.site_header = 'WordTest 后台管理'
 admin.site.site_title = 'WordTest 后台管理'
 
 admin.site.register(UserProfile, UserProfileAdmin)
-admin.site.register(Category,CategoryAdmin)
-admin.site.register(Word,WordAdmin)
-admin.site.register(Questionbank,QuestionbankAdmin)
-admin.site.register(Answer_record,Answer_recordAdmin)
-admin.site.register(Examination_results,Examination_resultsAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Word, WordAdmin)
+admin.site.register(Questionbank, QuestionbankAdmin)
+admin.site.register(Answer_record, Answer_recordAdmin)
+admin.site.register(Examination_results, Examination_resultsAdmin)
